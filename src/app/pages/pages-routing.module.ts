4495966a40c69a66ab305c2def7450980665e77a
Rafael Baron <baronrafael@hotmail.com>
2020-03-16 17:05:40 -0400

    feat: adding elements to home page

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PagesComponent } from './pages.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: PagesComponent,
+    /*children: [
+      { path: '', redirectTo: 'search', pathMatch: 'full' },
+    ]*/
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { SharedModule } from '../shared/shared.module';
 import { PagesRoutingModule } from './pages-routing.module';
-
+import { PagesComponent } from './pages.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [PagesComponent],
   imports: [
     CommonModule,
+    SharedModule,
     PagesRoutingModule
   ]
 })
