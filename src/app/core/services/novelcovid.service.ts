2343b6d46a514c9953c734854a51c5bf55ebd7be
Rafael Baron <baronrafael@hotmail.com>
2020-03-17 16:18:21 -0400

    feat: adding stats cards

--- src/app/core/services/novelcovid.service.ts
+++ src/app/core/services/novelcovid.service.ts
@@ -7,13 +7,24 @@ import { ApiService } from './api.service';
 export class NovelcovidService {
 
   private readonly getAllInfoUrl;
+  private readonly getCountriesInfoUrl;
+  private readonly getCountryInfoUrl;
 
   constructor(private http: ApiService) {
     this.getAllInfoUrl = 'all';
+    this.getCountriesInfoUrl = 'countries';
    }
 
   getAllInfo(){
     return this.http.get(this.getAllInfoUrl);
+  }
+
+  getCountriesInfo(){
+    return this.http.get(this.getCountriesInfoUrl);
+  }
+
+  getSpecificCountryInfo(country: string){
+    return this.http.get(this.getCountriesInfoUrl+'/'+country);
   }
 
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -9,6 +9,10 @@ import { NovelcovidService } from 'src/app/core/services/novelcovid.service';
 export class HomeComponent implements OnInit {
 
   generalInfo: any;
+  CountriesInfo: any;
+  totalCriticalCases: number;
+  todayCases: number;
+  todayDeaths: number;
 
   constructor(
     private novelCovid: NovelcovidService
@@ -16,6 +20,17 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
     this.getAllInfo();
+    this.getCountriesInfo();
+    if (navigator.geolocation) {
+      console.log("🗺️ yep, we can proceed!");
+      navigator.geolocation.getCurrentPosition(this.displayLocationInfo);
+    } else {
+      console.log("no can do");
+    }
+  }
+
+  displayLocationInfo(position) {
+    console.log(position.coords);
   }
 
   getAllInfo(){
@@ -30,6 +45,44 @@ export class HomeComponent implements OnInit {
         console.log(err);
       },
     );
+  }
+
+  getCountriesInfo(){
+    this.novelCovid.getCountriesInfo()
+    .subscribe(
+      (res) => {
+        //console.log(res)
+        this.CountriesInfo = res;
+        console.log(this.CountriesInfo);
+        this.calculateTodaysStats();
+      },
+      (err) => {
+        console.log(err);
+      },
+    );
+  }
+
+  getSpecificCountryInfo(country: string){
+    this.novelCovid.getSpecificCountryInfo(country)
+    .subscribe(
+      (res) => {
+        console.log(res)
+      },
+      (err) => {
+        console.log(err);
+      },
+    );
+  }
+
+  calculateTodaysStats(){
+    this.totalCriticalCases = 0;
+    this.todayCases = 0;
+    this.todayDeaths = 0;
+    for(let i = 0; i < this.CountriesInfo.length; i++){
+      this.totalCriticalCases = this.totalCriticalCases + this.CountriesInfo[i].critical;
+      this.todayCases = this.todayCases + this.CountriesInfo[i].todayCases;
+      this.todayDeaths = this.todayDeaths + this.CountriesInfo[i].todayDeaths;
+    }
   }
 
 }
