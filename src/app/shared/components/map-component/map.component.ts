74e64356eed5a4a68a52ed7b2db6a2b243b28efa
Alessandro Cuppari <alessandrojcuppari@gmail.com>
2020-03-22 02:54:53 -0400

    Refactor mapcomponent with ngxmapbox

--- src/app/shared/components/map-component/map.component.ts
+++ src/app/shared/components/map-component/map.component.ts
@@ -1,25 +1,45 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
-import { MAP_CONTAINER_NAME } from '@shared/models/constant';
-import { MapService } from '@shared/services';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MapComponent as MapGLComponent } from 'ngx-mapbox-gl';
+import { Map } from 'mapbox-gl';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss'],
-  providers: [
-    {
-      provide: MAP_CONTAINER_NAME,
-      useValue: 'mapContainer',
-    },
-    MapService,
-  ],
 })
-export class MapComponent implements OnInit, AfterViewInit {
-  constructor(private mapInstance: MapService) {}
+export class MapComponent implements OnInit {
+  private position: Position;
+  style = 'mapbox://styles/alessandrojcm/ck82fo7fu50j81ipckt4kyq3l';
+  zoom = 2;
 
-  ngOnInit(): void {}
+  @ViewChild(MapComponent) mapComponent!: MapGLComponent;
+  private map: Map;
 
-  ngAfterViewInit(): void {
-    this.mapInstance.findMe();
+  constructor() {}
+
+  ngOnInit(): void {
+    if (navigator.geolocation) {
+      console.log('🗺️ yep, we can find u! 😃');
+      navigator.geolocation.getCurrentPosition((pos) => {
+        this.position = pos;
+      });
+    } else {
+      console.log("Can't find u with navigator.geolocation 😞");
+    }
+  }
+
+  bindMap(map: Map) {
+    this.map = map;
+    if (!this.position) {
+      return;
+    }
+    this.map.setCenter([
+      this.position.coords.longitude,
+      this.position.coords.latitude,
+    ]);
+    this.map.zoomTo(9, {
+      duration: 3500,
+      animate: true,
+    });
   }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from './material/material.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MapComponent } from './components/map-component/map.component';
 import { GeneralInfoComponent } from '@shared/components/general-info/general-info.component';
+
+import { environment } from '@environments/environment';
+
+import { NgxMapboxGLModule } from 'ngx-mapbox-gl';
 
 @NgModule({
   declarations: [MapComponent, GeneralInfoComponent],
@@ -14,6 +18,9 @@ import { GeneralInfoComponent } from '@shared/components/general-info/general-in
     FormsModule,
     MaterialModule,
     FlexLayoutModule,
+    NgxMapboxGLModule.withConfig({
+      accessToken: environment.mapBoxToken,
+    }),
   ],
   exports: [
     ReactiveFormsModule,
