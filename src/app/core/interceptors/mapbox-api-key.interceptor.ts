54670ef0bd928770100496559c9ddf80bf3f0509
Alessandro Cuppari <alessandrojcuppari@gmail.com>
2020-03-22 15:02:51 -0400

    Imports refactor

--- src/app/core/interceptors/mapbox-api-key.interceptor.ts
+++ src/app/core/interceptors/mapbox-api-key.interceptor.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
 import {
-  HttpRequest,
-  HttpHandler,
   HttpEvent,
+  HttpHandler,
   HttpInterceptor,
   HttpParams,
+  HttpRequest,
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '@environments/environment';
