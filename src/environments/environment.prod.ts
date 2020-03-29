12dd16a44c0c9de415d1f6e0dd54d02ff88badd8
Alessandro Cuppari <alessandrojcuppari@gmail.com>
2020-03-28 22:10:41 -0400

    environment.prod fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,16 @@
 export const environment = {
-  production: false,
+  production: true,
   mapBox: {
-    token: '',
+    username: 'alessandrojcm',
+    token:
+      'pk.eyJ1IjoiYWxlc3NhbmRyb2pjbSIsImEiOiJjazgzbWw3MmMwMHluM2VtcmxoeWUwOW5iIn0.e6bdqayMgiuERemkcfWyWg',
+    geoJsonSource:
+      'https://raw.githubusercontent.com/bsrweb/geo-json-countries-mapbox/master/countries/all-countries.json',
     urls: {
-      datasets: 'https://api.mapbox.com/datasets/v1/',
+      datasets: 'https://api.mapbox.com/datasets/v1',
     },
   },
   novelCovidUrl: ' https://corona.lmao.ninja',
+  mapBackgroundColor: '#f5f5f5',
+  mapFillColor: '#000000',
 };
